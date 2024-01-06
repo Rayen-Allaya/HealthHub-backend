999d94a3303184428bc02a81de26b65fb796ce4e
malekgr <malek.garrach@supcom.tn>
2024-01-06 16:19:22 +0100

    appointments and availibilities

---
+++ app/Http/Controllers/AppointmentController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Appointment;
+use Illuminate\Http\Request;
+
+class AppointmentController extends Controller
+{
+    public function index()
+    {
+        $appointment = Appointment::all();
+        return $appointment;        
+    }
+}
