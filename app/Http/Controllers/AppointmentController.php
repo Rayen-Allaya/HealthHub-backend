b9f02a464bcafafd5e3ffd1712a8cc1f6af65643
Rayen-Allaya <allayarayen@gmail.com>
2024-01-07 23:16:26 +0100

    availibilities

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -3,15 +3,40 @@
 namespace App\Http\Controllers;
 
 use App\Models\Appointment;
+use App\Models\Availibility;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class AppointmentController extends Controller
 {
+
+    public function create($id)
+    {
+        $authUser = auth('sanctum')->user()->id;
+        $ava = Availibility::where("id", $id)->first();
+        $appointment = Appointment::create(["user_id" => $authUser, "doctor_id" => $ava->doctor_id, "datetime" => $ava->datetime]);
+        $ava->delete();
+        return $appointment;
+    }
+
     public function index()
     {
-        $appointment = Appointment::all();
-        return $appointment;        
+        $mytime = Carbon::now();
+        $mytime = $mytime->toDateTimeString();
+        $user = auth('sanctum')->user();
+        $appointment = Appointment::where("user_id", $user->id)->with(["doctor", "doctor.doctorProfile"])->where("datetime", ">=", $mytime)->orderBy("datetime", "asc")->get();
+        return response()->json($appointment, 200);
     }
+
+    public function history()
+    {
+        $mytime = Carbon::now();
+        $mytime = $mytime->toDateTimeString();
+        $user = auth('sanctum')->user();
+        $appointment = Appointment::where("user_id", $user->id)->with(["doctor", "doctor.doctorProfile"])->where("datetime", "<", $mytime)->orderBy("datetime", "desc")->get();
+        return response()->json($appointment, 200);
+    }
+
     public function update(Request $request, $id)
     {
         $appointments = Appointment::find($id);
@@ -25,7 +50,8 @@ class AppointmentController extends Controller
         return response()->json(['message' => 'appointment updated successfully', 'appointment' => $appointments], 200);
     }
 
-    public function delete($id){
+    public function delete($id)
+    {
         $appointments = Appointment::find($id);
         $appointments->delete();
         return response()->json(['message' => 'appointment deleted successfully'], 200);
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AvailibilityController;
 use App\Http\Controllers\AppointmentController;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DoctorProfileController;
 use App\Http\Controllers\ReviewController;
 
@@ -31,7 +32,14 @@ Route::delete('/reviews/{id}', [ReviewController::class, 'delete']);
 Route::get('/doctors/{doctor_id}/appointments', [AppointmentController::class, 'index']);
 Route::get('/doctors/{doctor_id}/availibilities', [AvailibilityController::class, 'index']);
 
-Route::get('/appointments/create', [AppointmentController::class, 'index']);
+Route::get("/appointments", [AppointmentController::class, 'index']);
+Route::get("/appointmentsHistory", [AppointmentController::class, 'history']);
+
+Route::get('/appointments/{id}/create', [AppointmentController::class, 'create']);
+
+
+
+// Route::get('/appointments/create', [AppointmentController::class, 'index']);
 Route::put('/appointments/update/{id}', [AppointmentController::class, 'update']);
 Route::delete('/appointments/delete/{id}', [AppointmentController::class, 'delete']);
 
@@ -45,3 +53,9 @@ Route::get('/doctors/{id}', [DoctorProfileController::class, 'getById']);
 Route::put('/doctors/update/{id}', [DoctorProfileController::class, 'update']);
 Route::delete('/doctors/delete/{id}', [DoctorProfileController::class, 'delete']);
 Route::get('/doctors/search', [DoctorProfileController::class, 'search']);
+
+//login user
+Route::post('/auth/login', [AuthController::class, 'loginUser'])->name('login');
+
+//logout user
+Route::post('auth/logout', [AuthController::class, 'logoutUser']);
